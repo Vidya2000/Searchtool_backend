3645e5c3bdeca99f70d99934a74fe9382e68e379
sai12021 <147162678+sai12021@users.noreply.github.com>
2024-05-06 23:34:11 +0530

    Add files via upload
    Added other functionality such as remove single/multiple, single insert. single edit

--- testting.py
+++ testting.py
@@ -48,12 +48,82 @@ def remove_searched_entry():
         with connection.cursor() as cursor:
             cursor.execute(f"""SET @search_id:=(SELECT search.id FROM search WHERE search.query={to_remove} 
             AND search.state!='Deleted')""")
-            cursor.execute("UPDATE search SET state = 'Deleted' WHERE search.id=@search_id")
+            cursor.execute("UPDATE search SET search.state = 'Deleted' WHERE search.id=@search_id")
             total_row_count = cursor.rowcount
             if total_row_count > 0:
                 return jsonify({'task': 'successful'})
             else:
                 return jsonify({'task': 'nothing to update'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        connection.close()
+
+
+@app.route('/multiple_remove_search', methods=['POST'])
+def remove_multiple_searched_entry():
+    connection = create_connection()
+    remove_search = request.form.get('multiple_remove_search')
+    remove_data = remove_search.replace(",", "','")
+    remove_search_data = f"""'{remove_data}'"""
+    connection.autocommit(True)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"""UPDATE search SET search.state = 'Deleted' WHERE search.id IN ({remove_search_data});""")
+            total_row_count = cursor.rowcount
+            if total_row_count > 0:
+                return jsonify({'task': 'successful'})
+            else:
+                return jsonify({'task': 'nothing to update'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        connection.close()
+
+
+@app.route('/insert_single_row', methods=['POST'])
+def insert_single_entry():
+    connection = create_connection()
+    row_insert_question = request.form.get('single_row_insert_question').lower()
+    row_insert_result = request.form.get('single_row_insert_result').lower()
+    connection.autocommit(True)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"""INSERT INTO search (query, result, state) 
+            VALUES ({row_insert_question}, {row_insert_result}, 'Added');""")
+            cursor.execute(f"""INSERT INTO search_history_new (query, result, state) 
+            VALUES ({row_insert_question}, {row_insert_result}, 'Added');""")
+            total_row_count = cursor.rowcount
+            if total_row_count == 2:
+                return jsonify({'task': 'successful'})
+            else:
+                return jsonify({'task': 'failed'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        connection.close()
+
+
+@app.route('/edit_single_row', methods=['POST'])
+def update_single_row():
+    connection = create_connection()
+    question = request.form.get('selected_question')
+    new_value = request.form.get('selected_result')
+    connection.autocommit(True)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"""SET @search_id:=(SELECT search.id FROM search WHERE search.query={question} AND 
+            search.state!='Deleted')""")
+            id_fetched = cursor.fetchone()
+            cursor.execute(f"""UPDATE search SET search.result = {new_value}, search.state = 'Updated' 
+            WHERE search.id={id_fetched}""")
+            cursor.execute(f"""INSERT INTO search_history_new (query, result, state) 
+            VALUES ({question}, {new_value}, 'Added');""")
+            total_row_count = cursor.rowcount
+            if total_row_count == 2:
+                return jsonify({'task': 'successful'})
+            else:
+                return jsonify({'task': 'failed'})
     except Exception as e:
         return jsonify({'error': str(e)})
     finally:
